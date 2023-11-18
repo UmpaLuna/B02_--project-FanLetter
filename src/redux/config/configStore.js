5b1ce871ff4572765cd4933d54fcdfe024afb357
최문길 <cmk0905@naver.com>
2023-11-18 22:43:07 +0900

    'complete'

--- src/redux/config/configStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
-import data from "../modules/Data";
-const rootReducer = combineReducers({
-  data,
-});
-const store = createStore(rootReducer);
-
-export default store;
